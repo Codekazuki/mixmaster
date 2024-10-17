900e2565ed246ba3414b87bba5bbd74c978eac50
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-10-17 08:43:55 +0100

    sprint

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className='nav-center'>
       <span className='logo'>MixMaster</span>
+      <NavLink to='/' className='nav-link'>
+        Home
+      </NavLink>
+      <NavLink to='about' className='nav-link'>
+        About
+      </NavLink>
+      <NavLink to='newsletter' className='nav-link'>
+        Newsletter
+      </NavLink>
     </nav>
   );
 };
