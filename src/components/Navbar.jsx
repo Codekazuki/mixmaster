c8658f4e77cdd544e9132a870bf82b34718caaee
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-10-17 06:41:31 +0100

    sprint

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className='nav-center'>
+      <span className='logo'>MixMaster</span>
+    </nav>
+  );
+};
+
+export default Navbar;
