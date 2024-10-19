6bad29d6b5b67999f81fc8f7b677ae0aaf813d43
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-10-19 20:49:42 +0100

    sprint

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/AboutPage";
 
 const About = () => {
   return (
-    <div>
-      <h1>About Page</h1>
-    </div>
+    <Wrapper>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+        voluptate fuga, beatae unde esse vitae numquam voluptatem recusandae,
+        quidem ex, ipsum temporibus magnam aspernatur non consequuntur alias
+        iste tempora aliquid animi reprehenderit omnis. Sequi fugit, ullam optio
+        ipsam architecto excepturi unde recusandae magnam incidunt placeat illo
+        velit quaerat commodi repellendus praesentium quasi reprehenderit neque
+        nisi nihil iusto fuga minima. Eos quae nemo eligendi necessitatibus
+        labore quaerat vitae id reprehenderit?
+      </p>
+    </Wrapper>
   );
 };
 
