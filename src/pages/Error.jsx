1e4eebfb8ea6b9f7040c9fb4e04062c1ae01c3f8
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-10-20 05:30:35 +0100

    sprint

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { Link, useRouteError } from "react-router-dom";
+import img from "../assets/not-found.svg";
 
 const Error = () => {
+  if (error === 404) {
+    return (
+      <div>
+        <h1>oops</h1>
+        <h4>There was an error</h4>
+        <img src={img} alt='not found' />
+        <Link to='/'>Go back home</Link>
+      </div>
+    );
+  }
   return <div>Error</div>;
 };
 
