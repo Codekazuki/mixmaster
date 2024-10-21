5eb0939d478789821dc495433c6c9bb02ffa6b35
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-10-21 09:57:31 +0100

    sprint

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,25 +1,31 @@
 import React from "react";
 import { Link, useRouteError } from "react-router-dom";
 import img from "../assets/not-found.svg";
+import Wrapper from "../assets/wrappers/ErrorPage";
 
 const Error = () => {
   const error = useRouteError();
   if (error.status === 404) {
     return (
-      <div>
-        <h1>oops</h1>
-        <h4>There was an error</h4>
-        <img src={img} alt='not found' />
-        <Link to='/'>Go back home</Link>
-      </div>
+      <Wrapper>
+        <div>
+          <img src={img} alt='not found' />
+          <h3>oops</h3>
+          <p>The page does not exist</p>
+
+          <Link to='/'>Go back home</Link>
+        </div>
+      </Wrapper>
     );
   }
   return (
-    <div>
-      <h1>something went wrong</h1>
-      <h4>check back later </h4>
-      <Link to='/'>Go back home</Link>
-    </div>
+    <Wrapper>
+      <div>
+        <h1>something went wrong</h1>
+        <h4>check back later </h4>
+        <Link to='/'>Go back home</Link>
+      </div>
+    </Wrapper>
   );
 };
 
