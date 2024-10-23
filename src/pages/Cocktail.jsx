e2efe7686b0064528a4d05a13bf22ab46ca55e9c
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-10-23 09:59:27 +0100

    sprint

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useLoaderData, Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/CocktailPage";
+
+const singleCocktailUrl =
+  "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
+export const loader = async ({ params }) => {
+  const { id } = params;
+  const { data } = await axios.get(`${singleCocktailUrl}${id}`);
+  return { id, data };
+};
 
 const Cocktail = () => {
-  return <div>Cocktail</div>;
+  const { id, data } = useLoaderData();
+  return <Wrapper>Cocktail</Wrapper>;
 };
 
 export default Cocktail;
